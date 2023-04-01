02dd5d7267c6b00341224c5206ba28e3b5efe55e
Emmanuel Heredia <herediiaa.e@gmail.com>
2023-04-01 00:59:53 -0300

    add new path redirect home in routerModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,12 @@ import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {
-    path: '',
+path:"",
+redirectTo:"/home",
+pathMatch:"full"
+  },
+  {
+    path: 'home',
     component: HomeComponent,
   },
   {
